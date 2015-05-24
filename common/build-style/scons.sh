e166270d06de78c82b633c42ed8f79bdcfcd0f2d
beefcurtains <beefcurtains@users.noreply.github.com>
2015-05-24 05:48:31 +0000

    New package: godot-1.1

---
+++ common/build-style/scons.sh
@@ -0,0 +1,13 @@
+#
+# This is a stub helper for templates using scons.
+#
+
+do_build() {
+	echo "You should override do_build() in your template." >&2
+	exit 1
+}
+
+do_install() {
+	echo "You should override do_install() in your template." >&2
+	exit 1
+}
